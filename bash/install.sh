299ccdc449f5e6185b234fe3d2822dbd8eaa33b3
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2025-02-23 18:02:00 +0100

    Install LLVM and clang with apt-get via install.sh

--- bash/install.sh
+++ bash/install.sh
@@ -25,3 +25,6 @@ git alias
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# install llvm and clang
+apt-get install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang
+
