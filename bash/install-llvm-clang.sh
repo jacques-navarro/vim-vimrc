0b3ee6d1d6b2ae7ec564a3f5db9e1eb82edd087d
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2025-02-27 00:15:07 +0100

    Second bash script must be called from same location as first
    The call to the second bash script; configure-git-vim.sh, must be made
    from the same working directory as the first script. If the file is not
    located in the same directory or the PWD is different then the file
    won't be found.

--- bash/install-llvm-clang.sh
+++ bash/install-llvm-clang.sh
@@ -9,6 +9,7 @@ echo '************************'
 apt-get -y install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang
 
 # run configure-git-vim.sh as regular user
-# requires $USER as command line parameter
-# when the current script is called
+# requires $USER as command line parameter when the current script is called
+# The second bash script and the PWD must be the same as the first script
+cd ~/vim/vim-vimrc/bash
 exec su "$1" -- "configure-git-vim.sh"
